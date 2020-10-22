0be7b526b7e51fbb7c0d8ac76222b2ed7f5d51ab
bradymomo <brady19961227@gmail.com>
2020-10-22 17:36:23 +0800

    feat: Add ASYNC_INIT_MIGRATION mode (#71)
    Let "pc.ram" not be sent in live migration time.
        Send a part of "pc.ram" in each epoch until the transmission is completed.
        This mode can reduce the system pause time in the live migration time.

--- kvm/include/linux/kvm_ft.h
+++ kvm/include/linux/kvm_ft.h
@@ -27,6 +27,7 @@ struct kvm_shmem_child;
 struct kvm_vcpu;
 struct kvm_vcpu_get_shared_all_state;
 struct kvmft_set_master_slave_sockets;
+struct kvm_shmem_page_not_diff_range;
 /**
  * kvmft_dirty_list : structure member of kvm_context
  * put_off = length of list
@@ -42,6 +43,8 @@ struct kvmft_dirty_list {
     __u32 *gva_spcl_pages;
 
     __u32 *spcl_bitmap;         // if set, the speculated page corresponding in pages is dirty
+    __u32 not_diff_start;
+    __u32 not_diff_end;
     __u32 pages[];
 };
 
@@ -165,6 +168,8 @@ int kvmft_vcpu_alloc_shared_all_state(struct kvm_vcpu *vcpu,
 void kvmft_gva_spcl_unprotect_page(struct kvm *kvm, unsigned long gfn);
 int kvmft_ioctl_set_master_slave_sockets(struct kvm *kvm,
     struct kvmft_set_master_slave_sockets *socks);
+
+int kvm_page_not_diff_range(struct kvm *kvm, struct kvm_shmem_page_not_diff_range range);
 
 #endif
 
